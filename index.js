10384d9824cd0a1052872118f744779084d505cc
Noah Harris <jnharris@boomtownroi.com>
2020-03-07 00:20:22 -0500

    Create main <-> render talk to dis/enable floating

--- index.js
+++ index.js
@@ -1,27 +1,53 @@
 const { app, BrowserWindow, ipcMain, globalShortcut } = require("electron")
+let floating = false
+let float_waiting = false
 
-function draw_root() {
-    const root = new BrowserWindow({
+async function draw_root(floating) {
+    let root = new BrowserWindow({
         webPreferences: {
             nodeIntegration: true
         },
         height: 200,
         width: 300,
         frame: false,
+        fullScreenable: !floating
     })
+
+    if (floating) {
+        root.setAlwaysOnTop(true, "floating", 1)
+        root.setVisibleOnAllWorkspaces(true)
+    }
+
     root.loadFile("./render/editor.html")
+    return root
 }
 
-async function float() {
-    console.log("we floatin");
+async function float_restart(content) {
+    ipcMain.on("float-ok", async () => {})
+
+    let active = BrowserWindow.getFocusedWindow()
+
+    let created = await draw_root(floating)
+    await created.webContents.send("editor-content", content)
+    await created.focus()
+
+    await active.close()
+
+}
+
+async function float_prepare() {
+    let active = BrowserWindow.getFocusedWindow()
+    floating = !floating
+    ipcMain.on("float-ok", float_restart)
+    active.webContents.send("float-prepare")
 }
 
 function register_handlers() {
     let gs = globalShortcut
-    gs.register("CmdOrCtrl+Shift+F", float)
+    gs.register("CmdOrCtrl+Shift+F", float_prepare)
 }
 
 app.on("ready", async () => {
-    draw_root()
+    draw_root(floating)
     register_handlers()
 })
--- render/render.js
+++ render/render.js
@@ -1,7 +1,9 @@
 const do_key = require("./key_funcs")
-const { ipcRenderer } = require("electron")
-const keymap = {}
+const { ipcRenderer} = require("electron")
 
+const editor = document.getElementById("editor-root")
+
+const keymap = {}
 async function map_key(event) {
     keymap[event.key.toLowerCase()] = event.type == "keydown"
 }
@@ -11,3 +13,14 @@ document.onkeydown = async (event) => {
     do_key(event)
     map_key(event)
 }
+
+async function prepare_float() {
+    ipcRenderer.send("float-ok", editor.value)
+}
+
+async function set_editor_content(content) {
+    editor.value = content
+}
+
+ipcRenderer.once("float-prepare", prepare_float)
+ipcRenderer.on("editor-content", set_editor_content)
