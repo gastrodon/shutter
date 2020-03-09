d98a4f30bc6a4b67a80d9a7daa4c1769e3982f4c
Zero <dakoolstwunn@gmail.com>
2020-03-09 01:04:06 -0400

    Create event functions namespace
    Create namespace for functions that should be called by events (ie
    ipcRenderer events)

---
+++ render/event_funcs.js
@@ -0,0 +1,18 @@
+const ui = require('./interface_funcs');
+
+async function prepare_float() {
+    let restore_data = {
+        content: editor.value.toString()
+    }
+    ipcRenderer.send("float-ok", restore_data)
+}
+
+async function editor_state(event, restore_data) {
+    editor.value = restore_data.content || ""
+    ui.set_handle_color(restore_data.floating)
+}
+
+module.exports = {
+    prepare_float,
+    editor_state
+}
--- render/render.js
+++ render/render.js
@@ -1,5 +1,5 @@
 const { do_key, map_key } = require("./key_funcs")
-const ui = require("./interface_funcs")
+const events = require("./event_funcs")
 const { ipcRenderer } = require("electron")
 
 const editor = document.getElementById("editor-root")
@@ -15,21 +15,10 @@ document.onkeydown = async (event) => {
     map_key(event)
 }
 
-async function prepare_float() {
-    let restore_data = {
-        content: editor.value.toString()
-    }
-    ipcRenderer.send("float-ok", restore_data)
-}
-
-async function editor_state(event, restore_data) {
-    editor.value = restore_data.content || ""
-    ui.set_handle_color(restore_data.floating)
-}
 
 // Should be called when the editor is about to toggle floating mode
 // This actually closes and redraws the window, so the editor may need
 // to do some cleanup and send some data over
-ipcRenderer.on("float-prepare", prepare_float)
+ipcRenderer.on("float-prepare", events.prepare_float)
 // Should be called when the main process wants the editor to load some state
-ipcRenderer.on("editor-state", editor_state)
+ipcRenderer.on("editor-state", events.editor_state)
